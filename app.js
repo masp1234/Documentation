ded67cfdc9dc1c48670a2225e1871a6bdb061a83
masp9 <mart2c04@stud.kea.dk>
2023-03-13 19:16:33 +0100

    login done

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 import express from 'express'
 import path from 'path'
-import validator from 'validator'
-import MarkdownIt from 'markdown-it'
-import fs from 'fs'
 
 const app = express()
 const PORT = 8080
-const md = MarkdownIt({
-    html: true,
-    linkify: true,
-    typographer: true
-})
+
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -28,7 +21,6 @@ app.get('/admin', (req, res) => {
 })
 
 app.post('/api/login', (req, res) => {
-    console.log(req.body)
     const { username, password } = req.body
     if (username === 'bob' && password === "123") {
         // kan ikke redirecte med res.redirect(url), da fetchData brokker sig over, at en html fil sendt med res.sendFile ikke er valid JSON. Prøv at finde ud af hvordan
@@ -38,20 +30,6 @@ app.post('/api/login', (req, res) => {
     else {
         return res.status(401).send({ message: 'Login failed' })
     }
-    // console.log(req.body)
-})
-
-app.post('/api/pages', (req, res) => {
-    const { pageName, pageContent } = req.body
-    const html = md.render(pageContent)
-    fs.writeFile(path.resolve(`public/pages/documentation/${pageName}.html`), html, error => {
-        if (error) {
-            console.log(error)
-        }
-    })
-
-    console.log(html)
-    return res.status(200).send( {message: 'success'} )
 })
 
 app.listen(PORT, error => {
